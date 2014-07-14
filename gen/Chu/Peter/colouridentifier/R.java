ed02f83f20067a022a314123406160f069572183
Peter Chu <cgpchu@uwaterloo.ca>
2014-07-13 21:05:02 -0400

    manifest fix

--- gen/Chu/Peter/colouridentifier/R.java
+++ gen/Chu/Peter/colouridentifier/R.java
@@ -27,20 +27,21 @@ public final class R {
         public static final int title=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
         public static final int button1=0x7f080004;
         public static final int cameraTextureView=0x7f080002;
-        public static final int colourBox=0x7f080007;
-        public static final int colourText=0x7f080006;
+        public static final int colourBox=0x7f080008;
+        public static final int colourText=0x7f080007;
         public static final int container=0x7f080000;
-        public static final int fivesecs=0x7f08000c;
+        public static final int fivesecs=0x7f08000d;
         public static final int frameLayout=0x7f080001;
-        public static final int manual=0x7f08000d;
-        public static final int onesec=0x7f080009;
+        public static final int logButton=0x7f080006;
+        public static final int manual=0x7f08000e;
+        public static final int onesec=0x7f08000a;
         public static final int refreshButton=0x7f080005;
-        public static final int refresh_menu=0x7f080008;
-        public static final int threesecs=0x7f08000b;
-        public static final int twosecs=0x7f08000a;
+        public static final int refresh_menu=0x7f080009;
+        public static final int threesecs=0x7f08000c;
+        public static final int twosecs=0x7f08000b;
         public static final int zoomControls=0x7f080003;
     }
     public static final class layout {
